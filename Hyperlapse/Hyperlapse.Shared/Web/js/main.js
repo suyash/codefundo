31d4ae0efbf6eab9194d32797f5de9dbef745b0e
Suyash Verma <dextrous93@gmail.com>
2014-07-20 08:28:03 +0530

    Set Up Map

--- Hyperlapse/Hyperlapse.Shared/Web/js/main.js
+++ Hyperlapse/Hyperlapse.Shared/Web/js/main.js
@@ -1,17 +1,72 @@
-﻿(function() {
+﻿(function(window, document, maps) {
 
     "use strict";
 
-    window.$ = document.querySelector;
+    var mapelement = null,
+        map = null,
+        markers = {
+            start: null,
+            end: null,
+            lookat: null
+        };
 
-    /**
-    Once again, the global context
-    */
-    var HL = {};
+    var init = function() {
+        
+        document.body.addEventListener("click", function () {
 
-    HL.showMap = function() {
+            console.log("click!!!");
+        });
 
-        sendMessage(Message.Local.MAP_SHOWN, {});
+        mapelement = document.querySelector("#map");
+
+        map = new maps.Map(mapelement, {
+            center: new maps.LatLng(29.8644, 77.8964),
+            zoom: 11,
+            streetViewControl: false
+        });
+
+        markers.start = new maps.Marker({
+            map: map,
+            draggable: true,
+            icon: '/images/markers/start.png',
+            title: 'Start Point'
+        });
+
+        markers.end = new maps.Marker({
+            map: map,
+            draggable: true,
+            icon: '/images/markers/end.png',
+            title: 'End Point'
+        });
+
+        markers.lookat = new maps.Marker({
+            map: map,
+            draggable: true,
+            icon: '/images/markers/lookat.png',
+            title: 'Lookat Point'
+        });
+    };
+
+    var setMarker = function(marker, position) {
+
+        marker.setPosition(position);
+        marker.setAnimation(maps.Animation.DROP);
+    };
+
+    var showMap = function(data) {
+
+        document.querySelector(".active").classList.remove("active");
+        document.querySelector("#mappercontainer").classList.add("active");
+
+        if (data.start) {
+        } else if (data.center) {
+        } else {
+
+            var center = map.getCenter();
+            setMarker(markers.start, center);
+            setMarker(markers.end, center);
+            setMarker(markers.lookat, center);
+        }
     };
 
     window.sendMessage = function (id, data) {
@@ -33,8 +88,10 @@
         switch (data.id) {
 
             case Message.Web.SHOW_MAP:
-                HL.showMap();
+                showMap(data);
                 break;
         }
     };
-})();
+
+    window.onload = init;
+})(window, document, google.maps);
--- Hyperlapse/Hyperlapse.Shared/pages/mapper/mapper.js
+++ Hyperlapse/Hyperlapse.Shared/pages/mapper/mapper.js
@@ -14,6 +14,7 @@
         ready: function (element, options) {
             
             $("#localcontenthost").addClass("onmapper");
+            HL.$appbar[0].winControl.show();
         },
 
         unload: function () {
