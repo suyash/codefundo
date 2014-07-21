4f29bc1bc0c9da622ded7df64c7f4c43f914ba62
Suyash Verma <dextrous93@gmail.com>
2014-07-21 14:08:54 +0530

    Add preview to mapper (Desktop)

--- Hyperlapse/Hyperlapse.Shared/pages/hub/hub.js
+++ Hyperlapse/Hyperlapse.Shared/pages/hub/hub.js
@@ -10,19 +10,30 @@
 
     WinJS.UI.Pages.define("/pages/hub/hub.html", {
 
+        /**
+        WinJS.UI.processAll has been called, return anything else t execute in a promise
+        */
         processed: function (element) {
 
+            /**
+            listview elements
+            */
             previewlist = $(".previewsection .previewlist > div")[0];
             locationlist = $(".mappersection .previewlist > div")[0];
 
+            /**
+            if either of the list is empty
+            */
             if (!HL.topPresets.length || !HL.topLocations.length) {
 
                 var presets = hyperlapseClient.getTable("presets"),
                     locations = hyperlapseClient.getTable("locations");
 
+                /**
+                get top presets from azure
+                */
                 var presetsPromise = presets.take(5).read().then(function(results) {
 
-                    //HL.topPresets.push(results);
                     for (var i = 0; i < results.length; i++) {
 
                         HL.topPresets.push(results[i]);
@@ -39,9 +50,10 @@
                     console.log("Cannot get top presets", error);
                 });
 
+                /**
+                get top locations from azure
+                */
                 var locationsPromise = locations.take(5).read().then(function (results) {
-
-                    //HL.topLocations.push(results);
 
                     for (var i = 0; i < results.length; i++) {
 
@@ -69,6 +81,9 @@
             }
         },
 
+        /**
+        after processed, do stuff related to things loaded after DOM
+        */
         ready: function (element, options) {
 
             if (!HL.isPhone) {
@@ -87,39 +102,96 @@
 
             hub.onloadingstatechanged = function (args) {
 
-                if (args.srcElement === hub.element && args.detail.loadingState === "complete") {
+                if (hub.loadingState === "complete") {
 
                     hub.onloadingstatechanged = null;
 
                     if (!HL.isPhone) {
 
-                        previewlist.winControl.onloadingstatechanged = function (args) {
+                        /**
+                        adds event listener to every item for mouse enter in preview list
+                        */
+                        var addPreviewListener = function() {
+                            
+                            $(".section1previewitem").listen("mouseenter", function (e) {
 
-                            if (previewlist.winControl.loadingState === "complete") {
+                                var index = previewlist.winControl.indexOfElement(e.target);
 
-                                $(".section1previewitem").listen("mouseenter", function (e) {
+                                var data = HL.topPresets.getAt(index);
 
-                                    var index = previewlist.winControl.indexOfElement(e.target);
+                                $(".previewsection .previewimage").setStyle("background-image", "url(\"" + data.previewimageurl + "\")");
+                            });
+                        };
 
-                                    var data = HL.topPresets.getAt(index);
+                        /**
+                        adds event listener to every item for mouse enter in mapper list
+                        */
+                        var addMapperListener = function () {
 
-                                    $(".previewsection .previewimage").setStyle("background-image", "url(\"" + data.previewimageurl + "\")");
-                                });
+                            $(".section2previewitem").listen("mouseenter", function (e) {
+
+                                var previewel = $(".mappersection .previewimage");
+
+                                /**
+                                returns Google static maps API url for the given location
+                                */
+                                var getStaticMapURL = function (d) {
+
+                                    var url = [
+                                        "http://maps.googleapis.com/maps/api/staticmap?center=",
+                                        data.centerlat,
+                                        ",",
+                                        d.centerlong,
+                                        "&zoom=",
+                                        15,
+                                        "&size=",
+                                        640,
+                                        "x",
+                                        640,
+                                        "&format=jpg&markers=color:green%7C",
+                                        d.centerlat,
+                                        ",",
+                                        d.centerlong
+                                    ];
+
+                                    return url.join("");
+                                };
+
+                                var index = locationlist.winControl.indexOfElement(e.target);
+
+                                var data = HL.topLocations.getAt(index);
+
+                                previewel.setStyle("background-image", "url(\"" + getStaticMapURL(data) + "\")");
+                            });
+                        };
+
+                        if (previewlist.winControl.loadingState === "complete") {
+
+                            addPreviewListener();
+                        } else {
+
+                            previewlist.winControl.onloadingstatechanged = function (args) {
+
+                                if (previewlist.winControl.loadingState === "complete") {
+
+                                    previewlist.winControl.onloadingstatechanged = null;
+                                    addPreviewListener();
+                                }
                             }
                         }
 
-                        locationlist.winControl.onloadingstatechanged = function (args) {
+                        if (locationlist.winControl.loadingState === "complete") {
 
-                            if (locationlist.winControl.loadingState === "complete") {
+                            addMapperListener();
+                        } else {
 
-                                $(".section2previewitem").listen("mouseenter", function (e) {
+                            locationlist.winControl.onloadingstatechanged = function(args) {
 
-                                    var index = previewlist.winControl.indexOfElement(e.target);
+                                if (locationlist.winControl.loadingState === "complete") {
 
-                                    var data = HL.topLocations.getAt(index);
-
-                                    //$(".previewsection .previewimage").setStyle("background-image", "url(\"" + data.previewimageurl + "\")");
-                                });
+                                    locationlist.winControl.onloadingstatechanged = null;
+                                    addMapperListener();
+                                }
                             }
                         }
                     } else {
@@ -130,15 +202,9 @@
             }
         },
 
-        unload: function () {
-            // TODO: Respond to navigations away from this page.
-        },
+        unload: function() {},
 
-        updateLayout: function (element) {
-            /// <param name="element" domElement="true" />
-
-            // TODO: Respond to changes in layout.
-        },
+        updateLayout: function (element) {},
 
         /**
         Called when mapper is invoked
